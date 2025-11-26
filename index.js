c43ddf4de3e9372872070b363bd58bd6c0ae906f
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-11-26 09:22:55 +0600

    product collection created and product add successful

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcejyck.mongodb.net/?appName=Cluster0`
-
+//middleware
+app.use(cors());
+app.use(express.json());
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -17,6 +20,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("banglaBazarDB");
+    const productsCollection = db.collection("products");
+    // GET ALL PRODUCTS
+    // ADD PRODUCT
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
